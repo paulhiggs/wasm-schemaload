2efb1da48d2c91badcba8ae330d233ef46fd66ff
Paul Higgs <paul_higgs@hotmail.com>
2025-09-02 05:26:55 +0100

    initial commit

---
+++ index.ts
@@ -0,0 +1,17 @@
+import { readFileSync } from "fs";
+import { XmlDocument, XsdValidator } from "libxml2-wasm";
+import { xmlRegisterFsInputProviders } from "libxml2-wasm/lib/nodejs.mjs";
+xmlRegisterFsInputProviders();
+
+const schema = readFileSync('./schema/dvbi_v7.0-with-hls-hbbtv.xsd', { encoding: "utf-8" });
+
+const document1 = readFileSync('./ospTest.xml', { encoding: "utf-8" });
+
+const validator = XsdValidator.fromDoc(XmlDocument.fromString(schema));
+
+try {
+	const res = validator.validate(XmlDocument.fromString(document1));
+}
+catch (err) {
+	console.dir(err)
+}
